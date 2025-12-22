52b5094e86d4946337dca9513db839accc39a1ed
ChickenIQ <emibenzar@gmail.com>
2025-12-23 01:09:34 +0200

    fix names

--- lib/discord/utils.ts
+++ lib/discord/utils.ts
@@ -1,17 +1,15 @@
 import { Message } from "discord.js";
 
 export const getMessageUsername = (message: Message) => {
-  return message.member?.nickname || message.author.username;
+  return message.member?.nickname || message.author.displayName || message.author.username;
 };
 
 export const getReferencedUsername = async (message: Message) => {
-  const referenceId = message.reference?.messageId;
-  if (!referenceId) return null;
+  const ref = message.reference?.messageId;
+  if (!ref) return null;
 
-  const ref = await message.channel.messages.fetch(referenceId);
-  if (!ref.author.bot) return getMessageUsername(ref);
-
-  return ref.author.username;
+  const msg = await message.channel.messages.fetch(ref);
+  return getMessageUsername(msg);
 };
 
 export const parseMessageContent = (message: Message) => {
